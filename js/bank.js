36d340f7ffc49408a72bbabac95da7bd0e5c1db1
unknown <sakifur35-2749@diu.edu.bd>
2022-08-22 23:52:34 +0600

    Deposit, withdraw, balance area using grid layout

--- js/bank.js
+++ js/bank.js
@@ -1,10 +1,11 @@
 document.getElementById('login-submit').addEventListener('click', function () {
+    // get user email 
     const emailField = document.getElementById('user-email');
     const userEmail = emailField.value;
-    // get userPassword
+    // get user Password
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
-
+    // check email and pass 
     if (userEmail == 'polar@baap.com' && userPassword == 'password') {
         window.location.href = 'banking.html';
     }
