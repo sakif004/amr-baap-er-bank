ed22941299077fa48c3312b744cf88738c4e5b0f
unknown <sakifur35-2749@diu.edu.bd>
2022-08-25 00:51:17 +0600

    Update deposit and balance and handle string add

--- js/banking.js
+++ js/banking.js
@@ -2,12 +2,27 @@
 document.getElementById('deposit-button').addEventListener('click', function () {
     // get the amount deposited 
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
-    console.log(depositAmount);
+    const newDepositAmountText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositAmountText);
 
     const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositAmount;
+    const previousDepositAmountText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositAmountText);
+
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
+    depositTotal.innerText = newDepositTotal;
+
+
+    // update account balance 
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
 
     // clear the deposti input field 
     depositInput.value = '';
+
 });
